4a7f3422a31421e4f74cc3918eed2ea8701a1991
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-24 16:16:25 +0530

    scikit modules installed

---
+++ train.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import average_precision_score
+from sklearn.ensemble import RandomForestClassifier
