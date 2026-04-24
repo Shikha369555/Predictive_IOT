f086c31c34b391b9570828c65d3009b3bed2e57c
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-24 16:18:20 +0530

    loaded factor

--- train.py
+++ train.py
@@ -4,3 +4,5 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import average_precision_score
 from sklearn.ensemble import RandomForestClassifier
+# Load data
+df = pd.read_csv("data.csv")
